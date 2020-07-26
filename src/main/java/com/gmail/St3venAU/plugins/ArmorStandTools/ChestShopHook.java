9bda4007e7aca3886d0c07ea3213bd178d7c4fe9
Aurelien30000 <aurelien.domino@gmail.com>
2020-07-26 22:52:57 +0200

    Fix ChestShopHook

--- src/main/java/com/gmail/St3venAU/plugins/ArmorStandTools/ChestShopHook.java
+++ src/main/java/com/gmail/St3venAU/plugins/ArmorStandTools/ChestShopHook.java
@@ -1,24 +1,25 @@
 package com.gmail.St3venAU.plugins.ArmorStandTools;
 
 import com.Acrobot.ChestShop.Events.PreTransactionEvent;
-import com.Acrobot.ChestShop.Events.TransactionEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class ChestShopHook implements Listener {
 
+    private final Main plugin;
+
     public ChestShopHook(Main plugin) {
+        this.plugin = plugin;
         PreTransactionEvent.getHandlerList().unregister(plugin); // Avoid multiple registers
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
     public void onPreTransactionEvent(PreTransactionEvent event) {
-        if (event.getTransactionType() == TransactionEvent.TransactionType.BUY) return;
-
-        if (ArmorStandTool.get(event.getClient()) != null)
-            event.setCancelled(PreTransactionEvent.TransactionOutcome.OTHER);
+        if (plugin.savedInventories.containsKey(event.getClient().getUniqueId())) {
+            event.setCancelled(true);
+        }
     }
 
 }
